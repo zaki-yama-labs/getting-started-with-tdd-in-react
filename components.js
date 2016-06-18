2d3813c89acfcead39f2e9ffa45d461433986713
zaki-yama <shingoyamazaki00@gmail.com>
2016-06-18 15:52:41 +0900

    テスト9-11が通るようになった

--- components.js
+++ components.js
@@ -33,9 +33,18 @@ InputArea.PropTypes = {
 
 export class BeerList extends Component {
   render() {
-    return <ul />;
+    return this.props.items ?
+      (<ul>
+        {this.props.items.map((item, index) => (
+          <li key={index}>{item}</li>
+        ))}
+      </ul>)
+    : null;
   }
 }
+BeerList.propTypes = {
+  items: React.PropTypes.array.isRequired
+};
 
 export class BeerListContainer extends Component {
   constructor(props) {
