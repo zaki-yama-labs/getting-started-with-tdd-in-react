e4a9047a47a5b454c2ea72df4a4e87795c5265ea
zaki-yama <shingoyamazaki00@gmail.com>
2016-06-18 14:55:21 +0900

    テスト7が通るようになった

--- components.spec.js
+++ components.spec.js
@@ -49,11 +49,10 @@ describe('InputArea', () => {
   });
 
   it('should accept input', () => {
-    const wrapper = shallow(<InputArea/>);
+    const wrapper = mount(<InputArea/>);
     const input = wrapper.find('input');
     input.simulate('change', { target: { value: 'Resin' } });
     expect(wrapper.state('text')).to.equal('Resin');
     expect(input.prop('value')).to.equal('Resin');
   });
 });
-
